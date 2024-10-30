2dc2133a56e0fe5c739d16380b0ac48b06357f37
joaquin <joaqui222@gmail.com>
2024-10-30 01:34:22 -0300

    feat: Creates tables, application startup and configuration routes.

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
-
+from .routes import users, products
+from .database import init_db
 
 app = FastAPI()
+
+# Llama a init_db() al inicio para crear las tablas en la base de datos
+@app.on_event("startup")
+def startup_event():
+    init_db()
+
+# Endpoint para verificar que la aplicación funciona
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API"}
+
+# Rutas 
+app.include_router(products)
+app.include_router(users)
