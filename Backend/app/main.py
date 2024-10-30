967c7923693d3b4c4047e028a41cce2f1613c8a4
joaquin <joaqui222@gmail.com>
2024-10-30 18:54:04 -0300

    feat: config cors origins

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import users, products
 from .database import init_db
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 app = FastAPI()
+
+frontend_url = os.getenv("FRONTEND_URL")
+
+# Configuro los orígenes permitidos
+origins = [
+    frontend_url,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 # Llama a init_db() al inicio para crear las tablas en la base de datos
 @app.on_event("startup")
